5f427788e9571817b57eb37653aab592b7a9feaf
tuchan2002 <trananhtu171002@gmail.com>
2023-08-23 17:27:42 +0700

    connect database

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,24 @@ import { AppService } from './app.service';
 import { UserModule } from './users/user.module';
 import { PostsModule } from './posts/posts.module';
 import { StoreModule } from './store/store.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule, PostsModule, StoreModule.forRoot()],
+  imports: [
+    UserModule, 
+    PostsModule, 
+    StoreModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3310,
+      username: 'root',
+      password: 'root',
+      database: 'nest_mysql',
+      entities: [],
+      // synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
